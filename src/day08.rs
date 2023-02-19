453c718984ff8da5445671afa1a91088d9a535f5
Martin Hofmann <m-hofmann@users.noreply.github.com>
2023-02-19 15:09:06 +0100

    Solve Day 08 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::fs;
 use std::str::FromStr;
 
@@ -33,6 +34,63 @@ fn is_visible_naive(grid: Vec<Vec<u8>>, x: usize, y: usize) -> bool {
     }
 }
 
+fn get_scenic_score(grid: Vec<Vec<u8>>, x: usize, y: usize) -> usize {
+    // max grid size (inclusive)
+    let max_y = grid.len()-1;
+    let max_x = grid[0].len()-1;
+
+    if x == 0 || x == max_x
+        || y == 0 || y == max_y {
+        // edge trees are always visible
+        return 0;
+    } else {
+        // TODO recurse & memoize for speedup
+        let height = grid[y][x];
+
+
+        let mut dist_from_left = 0;
+        let mut dist_from_right= 1;
+        for (i, &other_height) in grid[y].iter().enumerate() {
+            if i < x {
+                if other_height >= height {
+                    dist_from_left = 0;
+                }
+                dist_from_left += 1;
+            } else if i > x {
+                if other_height >= height {
+                    break;
+                }
+                // at edge -> no more trees in view -> do not inc counter
+                if i != max_x {
+                    dist_from_right += 1;
+                }
+            }
+        }
+
+        let mut dist_from_top = 0;
+        let mut dist_from_bottom = 1;
+        for (i, line) in grid.iter().enumerate() {
+            if i < y {
+                if line[x] >= height {
+                    dist_from_top = 0;
+                }
+                dist_from_top += 1
+            } else if i > y {
+                if line[x] >= height {
+                    break;
+                }
+                // at edge -> no more trees in view -> do not inc counter
+                if i != max_y {
+                    dist_from_bottom += 1;
+                }
+            }
+        }
+
+        let scenic_score = dist_from_left * dist_from_right * dist_from_top * dist_from_bottom;
+        return scenic_score;
+    }
+}
+
 pub fn day08() {
     println!("starting day 08");
 
@@ -49,7 +107,6 @@ pub fn day08() {
         }
         grid.push(line_conv);
     }
-
     println!("Grid: \n{:?}", grid);
 
     let mut visible_count = 0;
@@ -61,8 +118,14 @@ pub fn day08() {
             }
         }
     }
-
     println!("Number of visible trees {:?}", visible_count);
 
+    let mut top_score = 0;
+    for (i, _) in grid.iter().enumerate() {
+        for (j, _) in grid[i].iter().enumerate() {
+            top_score = max(top_score, get_scenic_score(grid.clone(), j, i));
+        }
+    }
+    println!("Top scenic score: {:?}", top_score);
 }
 

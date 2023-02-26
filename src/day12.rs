2e77a031d36021273505449c8ccec8e827d75960
Martin Hofmann <m-hofmann@users.noreply.github.com>
2023-02-26 17:41:15 +0100

    Solve day 12 part 1

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,5 @@
-use std::{fs};
 use std::collections::{HashMap, HashSet};
+use std::fs;
 
 use itertools::Itertools;
 
@@ -21,15 +21,24 @@ fn coord_to_direction(pred: &(usize, usize), succ: &(usize, usize)) -> char {
     }
 }
 
-fn chardiff(a: &(usize, usize), b: &(usize, usize), grid: &Vec<Vec<char>>) -> i32 {
-    let a_val = get_normalized_value(grid[a.0][a.1]);
-    let b_val = get_normalized_value(grid[b.0][b.1]);
+fn is_valid_chardiff(from: &(usize, usize), to: &(usize, usize), grid: &Vec<Vec<char>>) -> bool {
+    let from_val = get_normalized_value(grid[from.0][from.1]);
+    let to_val = get_normalized_value(grid[to.0][to.1]);
 
-    if (a.0 as i32 - b.0 as i32).abs() > 1 || (a.1 as i32 - b.1 as i32).abs() > 1 {
-        panic!("Not neighbors: {:?}, {:?}", a, b);
+    if (from.0 as i32 - to.0 as i32).abs() > 1 || (from.1 as i32 - to.1 as i32).abs() > 1 {
+        panic!("Not neighbors: {:?}, {:?}", from, to);
     }
 
-    return (a_val - b_val).abs();
+    let diff = to_val as i32 - from_val as i32;
+    if diff == 1 {
+        return true;
+    } else if diff > 1 {
+        return false
+    } else {
+        // need to read instructions carefully or spend hours debugging:
+        // (This also means that the elevation of the destination square can be much lower than the elevation of your current square.)
+        return true;
+    }
 }
 
 fn print_map(map: &Vec<Vec<char>>) {
@@ -41,7 +50,12 @@ fn print_map(map: &Vec<Vec<char>>) {
     }
 }
 
-fn reconstruct_shortest_path(start: &(usize, usize), target: &(usize, usize), cheapest_predecessor: &HashMap<(usize, usize), (usize, usize)>, grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn reconstruct_shortest_path(
+    start: &(usize, usize),
+    target: &(usize, usize),
+    cheapest_predecessor: &HashMap<(usize, usize), (usize, usize)>,
+    grid: &Vec<Vec<char>>,
+) -> Vec<Vec<char>> {
     let mut path = vec![vec!['.'; grid[0].len()]; grid.len()];
     let table = cheapest_predecessor.clone();
     let mut curr = target;
@@ -81,7 +95,7 @@ fn neighbors(&(y, x): &(usize, usize), grid: &Vec<Vec<char>>) -> Vec<(usize, usi
 pub fn day12() {
     println!("starting day 12");
 
-    let contents = fs::read_to_string("data/12_demo.txt").expect("Could not read file");
+    let contents = fs::read_to_string("data/12_input.txt").expect("Could not read file");
     let lines = contents.split('\n');
 
     let mut grid: Vec<Vec<char>> = vec![];
@@ -105,11 +119,11 @@ pub fn day12() {
         println!()
     }
 
-
     // to simplify addressing nodes, we use a tuple of vec indices as coordinates
     // (y_axis, x_axis) in
-    let mut discovered: HashSet<(usize, usize)> = HashSet::new();
-    discovered.insert(start);
+    let mut candidates: HashSet<(usize, usize)> = HashSet::new();
+    candidates.insert(start);
+    let mut visited : HashSet<(usize, usize)> = HashSet::new();
     let mut cheapest_predecessor: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
     let mut cost: HashMap<(usize, usize), u32> = HashMap::new();
     cost.insert(start, 0);
@@ -121,35 +135,35 @@ pub fn day12() {
 
     print_map(&map);
 
-    while !discovered.is_empty() {
-        let discovered_clone = discovered.clone();
-        let current = discovered_clone
+    while !candidates.is_empty() {
+        let candidates_clone = candidates.clone();
+        let current = candidates_clone
             .iter()
             .map(|it| ((it), cost.get(it).unwrap_or(&u32::MAX)))
             .sorted_by_key(|x| x.1)
             .map(|x| x.0)
-            .min()
-            .unwrap();
+            .next().unwrap();
 
         if *current == target {
             println!("Reached target");
             break;
         }
 
-        discovered.remove(current);
+        candidates.remove(current);
+        visited.insert(*current);
 
         for neighbor in neighbors(current, &grid) {
-            let chardiff = chardiff(current, &neighbor, &grid);
+            let valid_field = is_valid_chardiff(current, &neighbor, &grid);
             let new_dist = cost[current] + 1;
-            if new_dist < *cost.get(&neighbor).unwrap_or(&u32::MAX) && chardiff <= 1 {
+            if new_dist < *cost.get(&neighbor).unwrap_or(&u32::MAX)
+                && valid_field
+                {
                 map[current.0][current.1] = coord_to_direction(current, &neighbor);
                 cheapest_predecessor.insert(neighbor, *current);
                 cost.insert(neighbor, new_dist);
-                discovered.insert(neighbor);
+                candidates.insert(neighbor);
             }
         }
-        println!("All shortest paths:");
-        print_map(&map);
     }
 
     println!("All shortest paths:");
